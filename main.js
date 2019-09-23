f06694aab86a32d3c880523a5a1e4afa6b89fae1
vikram080401 <52628485+vikram080401@users.noreply.github.com>
2019-09-23 01:28:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello NodeApp!! \n");
-  response.end("This project was created to demonstrate the below:  \n");
-  response.end("HTQual Technology Solutions hired you as a MEAN Stack Developer. The organization decided to implement DevOps to develop and deliver the products. Since HTQual is an Agile organization, they follow Scrum methodology to develop the projects incrementally. The Company decided to develop their website on Mean stack. Since you are the MEAN stack developer, you have to demonstrate that deploying the Angular application on Docker is always the best approach to develop a project and test it incrementally");
+  response.end("Hello NodeApp!! This project was created to demonstrate the below : HTQual Technology Solutions hired you as a MEAN Stack Developer. The organization decided to implement DevOps to develop and deliver the products. Since HTQual is an Agile organization, they follow Scrum methodology to develop the projects incrementally. The Company decided to develop their website on Mean stack. Since you are the MEAN stack developer, you have to demonstrate that deploying the Angular application on Docker is always the best approach to develop a project and test it incrementally");
 });
 
 // listen on localhost:8000
